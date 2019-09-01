fe0379e74cd3ab21c9ac04a6395868c0b3254c19
ltdbyte <54761232+ltdbyte@users.noreply.github.com>
2019-09-01 18:18:45 +1000

    Add files via upload

---
+++ 15SwtichStatement.php
@@ -0,0 +1,23 @@
+<?php
+
+$number = 1;
+
+switch ($number) {
+	case 1:
+		echo 'One';
+	break;
+	
+	case 2:
+		echo 'Two';
+	break;
+	
+	case 3:
+		echo 'Three';
+	break;
+	
+	default:
+		echo 'Number not found';
+	break;
+}
+
+?>
